213e7b5bac57072a4e72e80f266be29375dbcf8f
Kevin Sarria <reysabueso256@gmail.com>
2022-07-02 17:01:14 -0500

    Avances curso js Apartado de Node.js

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,29 @@ import express, { Router }  from "express";
 const router = express.Router();
 
 router.get( '/', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
-    res.render('inicio');
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
 });
 
 router.get( '/nosotros', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
     
-    const viajes = 'Viaje a Alemania';
-    
     res.render('nosotros', {
-        viajes
+        pagina: 'Nosotros'
+    });
+});
+
+router.get( '/viajes', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get( '/testimoniales', (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+    
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
     });
 });
 
